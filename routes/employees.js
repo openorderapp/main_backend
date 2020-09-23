6588ec52788f911b2364fd412542b7fbeb386110
ZiWei Rao <r3033595562@gmail.com>
2020-09-23 18:58:07 -0400

    Create a JWT login/register system on employee_auth.js, which is put on the auth folder

--- routes/employees.js
+++ routes/employees.js
@@ -1,8 +1,25 @@
-const 
-    RouteModel = require("./model/model"),
+const
+    express = require('express'),
+    config = require('../config'),
+    knex = require('knex')(config),
+    { register, login, authenticateToken } = require('../auth/employee_auth');
+
+const
     MODEL_NAME = 'employees',
     MODEL_ID = 'employee_id',
-    employee_model = new RouteModel(MODEL_NAME, MODEL_ID)
+    router = express.Router();
 
+router.get('/', authenticateToken, async (req, res) => {
+    try {
+        const query_result = (await knex(MODEL_NAME).where(MODEL_ID, req.employee.id).select())[0];
+        res.json(query_result);
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
 
-module.exports = employee_model.model_router;
+router.post('/register', register);
+
+router.post('/login', login);
+
+module.exports = router;
